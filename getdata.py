9af88f476bf715b5fe554ca91e14cf0d06ecc32f
Cam Appel <cameron@ourworldindata.org>
2022-05-16 22:03:15 +0100

    add(data)

--- getdata.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-url = "https://docs.google.com/spreadsheets/d/1sC2qSPnBy7iXco0OyUCqQGBhysWysFPiFTZQ0Wt6DQk/gviz/tq?tqx=out:csv&sheet=All+data"
-all_data = pd.read_csv(url)
-all_data.to_csv("all_data.csv", on_bad_lines="skip")
